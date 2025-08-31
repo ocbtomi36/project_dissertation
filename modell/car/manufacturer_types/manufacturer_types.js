8eb36527f923a203476ee7322ddc9913f8c0457b
ocbtomi36 <ocbtomi36@outlook.hu>
2025-08-31 11:05:21 +0200

    manufacturer_types modell is created and added to insert methods

---
+++ modell/car/manufacturer_types/manufacturer_types.js
@@ -0,0 +1,29 @@
+const db = require('../../database/db');
+
+module.exports = class ManufacturerTypes {
+
+    constructor(typesId,manufacturerId){
+        this.typesId = typesId,
+        this.manufacturerId = manufacturerId
+    }
+
+    async saveManufacturerTypes(){
+            try {
+                const [result] = await db.execute('INSERT INTO manufacturer_types (types_idtype, manufacturers_idmanufacturer) VALUES (? , ?);',
+                    [this.typesId, this.manufacturerId]);
+                    return result.insertId;
+                } catch (error) {
+                    console.error('There is an error in database:', error);
+                    throw error;
+                }
+            }
+    static async getFuelByFuel(types_idType,manufacturer_Idmanufacturer){
+        try {
+            const [row] = await db.query('SELECT * FROM manufacturer_types where types_idtype = ? and manufacturers_idmanufacturer = ?',[types_idType,manufacturer_Idmanufacturer]);
+                return row.length > 0 ? row[0] : null;
+        } catch (error) {
+            console.error('There is an error in database:', error);
+                throw error;
+            }
+    }
+}
