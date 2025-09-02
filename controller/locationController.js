37101a776e9f9d5da115d5efee496233f99dea69
ocbtomi36 <118051723+ocbtomi36@users.noreply.github.com>
2025-09-02 13:52:01 +0200

    location update is ready

--- controller/locationController.js
+++ controller/locationController.js
@@ -21,22 +21,20 @@ exports.getOneLocation = async (req,res,next) => {
 }
 exports.updateLocation = async (req,res,next) => {
     try {
-        const idlocation = req.params.idlocation;
+        const { idlocation } = req.params;
         const { location_name, phone_number,locality_name, postal_code, street_name, street_type, house_number } = req.body;
         const fkAddress = await AddressService.insertAddress(locality_name, postal_code, street_name, street_type, house_number);
         const dbLocationNameLocationId = await Location.getLocationByLocationName(location_name);
         const dbPhoneNumberLocationId = await Location.getPhoneNumByPhoneNum(phone_number);
-        const isLocationNameOk = dbLocationNameLocationId === null || dbLocationNameLocationId == idlocation;
-        const isPhoneNumberOk = dbPhoneNumberLocationId === null || dbPhoneNumberLocationId == idlocation;
-
+        const isLocationNameOk = dbLocationNameLocationId === null || dbLocationNameLocationId.idlocation == idlocation;
+        const isPhoneNumberOk = dbPhoneNumberLocationId === null || dbPhoneNumberLocationId.idlocation == idlocation;
         if (isLocationNameOk && isPhoneNumberOk) {
-            const updatingUser = new User(location_name,phone_number,fkAddress);
-            await updatingUser.updateUserData(idlocation);
+            const updatingLocation = new Location(location_name,phone_number,fkAddress);
+            await updatingLocation.updateLocation(idlocation);
             return res.status(201).json({ message: 'Location update is success' });
         }
 
         return res.status(401).json({ message: 'Location name or phone number must be unique' });
-        res.status(200).json({ message: 'Location is Updated' });
     } catch (error) {
         res.status(500).json({message: error.message})
     }
